a60a5b6da48cbc11f782f8b0f3993969e860289c
downzer0 <github@chrisrodriguez.me>
2017-07-28 21:36:52 -0400

    fix: simplifying states

--- src/Tab.js
+++ src/Tab.js
@@ -4,34 +4,16 @@ class Tab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false,
-      selected: false
+      expanded: this.props.index === this.props.selectedIndex,
+      selected: this.props.index === this.props.selectedIndex
     };
-
-    this.className = this.className.bind(this);
-  }
-
-  className() {
-    if (this.props.index === this.props.selectedIndex) {
-      this.setState({
-        expanded: true,
-        selected: true
-      });
-      return 'dz0-tab is-selected';
-    } else {
-      this.setState({
-        expanded: false,
-        selected: false
-      });
-      return 'dz0-tab';
-    }
   }
 
   render() {
     return (
       <button
         role="tab" id={"tab-" + this.props.index}
-        className={this.className()}
+        className={(this.props.index === this.props.selectedIndex) ? 'dz0-tab is-selected' : 'dz0-tab'}
         aria-selected={this.state.selected}
         aria-expanded={this.state.expanded}
         aria-controls={"tabpanel-" + this.props.index}
--- src/Tabpanel.js
+++ src/Tabpanel.js
@@ -4,34 +4,16 @@ class Tabpanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false,
-      hidden: true
+      expanded: this.props.index === this.props.selectedIndex,
+      hidden: !this.props.index === this.props.selectedIndex
     };
-
-    this.className = this.className.bind(this);
-  }
-
-  className() {
-    if (this.props.index === this.props.selectedIndex) {
-      this.setState({
-        expanded: true,
-        selected: true
-      });
-      return 'dz0-tabpanel is-selected';
-    } else {
-      this.setState({
-        expanded: false,
-        selected: false
-      });
-      return 'dz0-tabpanel';
-    }
   }
 
   render() {
     return (
       <div
         role="tabpanel" id={"tabpanel-" + this.props.index}
-        className={this.className()}
+        className={(this.props.index === this.props.selectedIndex) ? 'dz0-tabpanel is-selected' : 'dz0-tabpanel'}
         aria-expanded={this.state.expanded}
         aria-hidden={this.state.hidden}
         tabIndex={(this.props.selectedIndex === this.props.index) ? '0' : -1}>
